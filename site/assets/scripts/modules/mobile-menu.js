7cc2802431c735d2f46b9987f550693caca6192e
Johannes <johannes_fleischhut@web.de>
2020-05-15 00:51:02 +0200

    Footer gestyled

--- site/assets/scripts/modules/mobile-menu.js
+++ site/assets/scripts/modules/mobile-menu.js
@@ -3,11 +3,18 @@ class MobileMenu {
         this.menuIcon = document.querySelector(".site-header__menu-icon")
         this.menuContent = document.querySelector(".site-header__menu-content")
         this.siteHeader = document.querySelector(".site-header")
+        this.navLink = document.querySelectorAll(".primary-nav__link")
         this.events()
     }
 
     events() {
+        /* Menü-Icon Klick*/
         this.menuIcon.addEventListener("click", ()=> this.toggleTheMenu())
+        
+        /* Jeglicher Klick auf Nav-Liste*/
+        for (const link of this.navLink) {
+            link.addEventListener("click", ()=> this.toggleTheMenu())
+          }
     }
 
     // Methods
@@ -15,7 +22,6 @@ class MobileMenu {
         this.menuContent.classList.toggle("site-header__menu-content--is-visible");
         this.siteHeader.classList.toggle("site-header--is-expanded");
         this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
-
     }
 }
 
