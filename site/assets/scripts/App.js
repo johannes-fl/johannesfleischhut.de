2cc1503f14bc31d859220946199d3bce3d90cb23
Johannes <johannes_fleischhut@web.de>
2020-05-19 00:17:34 +0200

    Audio Styling angefangen

--- site/assets/scripts/App.js
+++ site/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/mobile-menu"
+import Accordion from "./modules/accordion"
 
 let mobileMenu = new MobileMenu();
+let accordion = new Accordion();
 
 if(module.hot) {
         module.hot.accept();
