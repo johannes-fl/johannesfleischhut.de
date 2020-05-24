1878fe521717e5e34d3529d89f9b28cba764fa90
Johannes <johannes_fleischhut@web.de>
2020-05-24 23:51:19 +0200

    Audio Player angefangen

--- site/assets/scripts/modules/audio-player.js
+++ site/assets/scripts/modules/audio-player.js
@@ -3,6 +3,9 @@ class AudioPlayer {
         this.audioFile = document.querySelectorAll(".audio__audio-file")
         this.playButton = document.querySelectorAll(".audio__control--play")
         this.pauseButton = document.querySelectorAll(".audio__control--pause")
+        this.replayButton = document.querySelectorAll(".audio__control--replay")
+        this.forwardButton = document.querySelectorAll(".audio__control--forward")
+        this.hasEnded = document.querySelectorAll("audio")
         this.events()
     }
 
@@ -13,18 +16,57 @@ class AudioPlayer {
         this.pauseButton.forEach(el => {
             el.addEventListener("click", e => this.pauseAudio(el))
         })
-        
+        this.replayButton.forEach(el => {
+            el.addEventListener("click", e => this.replayAudio(el))
+        })
+        this.forwardButton.forEach(el => {
+            el.addEventListener("click", e => this.forwardAudio(el))
+        }) 
+        this.hasEnded.forEach(el => {
+            el.addEventListener("ended", e => this.stopAudio(el))
+        })
     }
 
     playAudio(el) {
-            el.classList.remove("audio__control--active")
-            el.parentElement.querySelector(".audio__control--pause").classList.add("audio__control--active")
+        var audio = el.parentElement.querySelector("audio")
+        
+        el.classList.remove("audio__control--active")
+        el.parentElement.querySelector(".audio__control--pause").classList.add("audio__control--active")
+        audio.play()
     }
 
     pauseAudio(el) {
+        var audio = el.parentElement.querySelector("audio")
+
         el.classList.remove("audio__control--active")
         el.parentElement.querySelector(".audio__control--play").classList.add("audio__control--active")
-}
+        audio.pause()
+    } 
+
+    stopAudio(el) {
+        el.parentElement.parentElement.querySelector(".audio__control--play").classList.add("audio__control--active")
+        el.parentElement.parentElement.querySelector(".audio__control--pause").classList.remove("audio__control--active")
+    }
+
+    replayAudio(el) {
+        var audio = el.parentElement.querySelector("audio")
+
+        el.classList.remove("audio__control--replay--active")
+        void el.offsetWidth
+        el.classList.add("audio__control--replay--active")
+        audio.currentTime -= 10;
+    }
+
+    forwardAudio(el) {
+        var audio = el.parentElement.querySelector("audio")
+
+        el.classList.remove("audio__control--forward--active")
+        void el.offsetWidth
+        el.classList.add("audio__control--forward--active")
+        audio.currentTime += 10;
+    }
+
+
 }
 
 export default AudioPlayer;
