a72f4e815b38432c2ef2cdad357a8e2a049c5994
Johannes <johannes_fleischhut@web.de>
2020-05-25 22:44:01 +0200

    Audio Player weitergearbeitet

--- site/assets/scripts/modules/audio-player.js
+++ site/assets/scripts/modules/audio-player.js
@@ -5,7 +5,7 @@ class AudioPlayer {
         this.pauseButton = document.querySelectorAll(".audio__control--pause")
         this.replayButton = document.querySelectorAll(".audio__control--replay")
         this.forwardButton = document.querySelectorAll(".audio__control--forward")
-        this.hasEnded = document.querySelectorAll("audio")
+        this.audio = document.querySelectorAll("audio")
         this.events()
     }
 
@@ -22,16 +22,25 @@ class AudioPlayer {
         this.forwardButton.forEach(el => {
             el.addEventListener("click", e => this.forwardAudio(el))
         }) 
-        this.hasEnded.forEach(el => {
+        this.audio.forEach(el => {
             el.addEventListener("ended", e => this.stopAudio(el))
+        })
+    }
+
+    stopOtherAudio() {
+        this.audioFile.forEach(el => {
+            el.querySelector(".audio__control--play").classList.add("audio__control--active")
+            el.querySelector(".audio__control--pause").classList.remove("audio__control--active")
+            el.querySelector("audio").pause()
         })
     }
 
     playAudio(el) {
         var audio = el.parentElement.querySelector("audio")
-        
+        this.stopOtherAudio()
         el.classList.remove("audio__control--active")
         el.parentElement.querySelector(".audio__control--pause").classList.add("audio__control--active")
+        el.parentElement.querySelector("P").classList.add("audio__title--move")
         audio.play()
     }
 
@@ -40,6 +49,7 @@ class AudioPlayer {
 
         el.classList.remove("audio__control--active")
         el.parentElement.querySelector(".audio__control--play").classList.add("audio__control--active")
+        el.parentElement.querySelector("P").classList.remove("audio__title--move")
         audio.pause()
     } 
 
